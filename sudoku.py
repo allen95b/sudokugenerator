0478bdf1137ad2d52e34d9f034f809202216b7e1
Brendan Allen <b.james.allen95@gmail.com>
2021-02-11 13:32:55 -0600

    First Commit. Added function for difficulty selection, along with user input

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,14 @@
 print("Hello friends! Welcome to my sudoku generator. Enjoy!")
+
+difficulty = input("Please Input Your Difficulty (Easy, Medium, Hard): ")
+
+def sudoku_diff():
+    if difficutly.lower() == "easy":
+        print("E")
+    elif difficulty.lower() == "medium":
+        print("M")
+    elif difficulty.lower() == "hard":
+        print("H")
+    else:
+        print("Invalid Entry! \n Please start the program again. Goodbye. ")
+        quit()
